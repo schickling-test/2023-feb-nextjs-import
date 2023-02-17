21e8b2d96e44e62be63697acc55ce6bac6d5bb5d
Johannes Schickling <schickling.j@gmail.com>
2023-02-17 16:01:40 +0100

    add bundle

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 export default async function Home() {
   const mod = await import('./example.mjs');
+
+  console.log(mod);
   
   return (
     <main>
       <h1>Testing await import</h1>
-      <p>{mod.example()}</p>
     </main>
   )
 }
